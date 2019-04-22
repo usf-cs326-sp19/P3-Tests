31d7c9f3ef7e9933397ef682921ee1b20fa55671
Matthew Malensek <matt@malensek.net>
2019-04-22 13:08:38 -0700

    Fix really dumb error

--- 01-Unix-Utilities-1.sh
+++ 01-Unix-Utilities-1.sh
@@ -3,7 +3,6 @@ source "${TEST_DIR}/funcs.bash"
 test_start "Unix Utilities" \
     "Runs 'ls /', 'df', and 'w' with custom memory allocator"
 
-rm ./allocator.so
 # Check to make sure the library exists
 [[ -e "./allocator.so" ]] || test_end 1
 
