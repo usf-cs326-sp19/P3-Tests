d1f9c43032097e06fd737cd42a675c77da3b5307
Matthew Malensek <matt@malensek.net>
2019-04-22 11:15:24 -0700

    Renumber thread safety

--- 09-Thread-Safety-1.sh
+++
@@ -1,9 +0,0 @@
-source "${TEST_DIR}/funcs.bash"
-
-test_start "Thread Safety" \
-    "Performs allocations across multiple threads in parallel"
-
-# If this crashes or times out, your allocator is not thread safe!
-LD_PRELOAD=./allocator.so tests/progs/thread-safety
-
-test_end
