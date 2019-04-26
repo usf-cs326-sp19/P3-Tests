b1225493ffb89d66ecf533e9f46d6abb6a2ee89f
Matthew Malensek <matt@malensek.net>
2019-04-26 15:02:02 -0700

    No scribbling for realloc()

--- progs/scribble.c
+++ progs/scribble.c
@@ -30,12 +30,15 @@ int main(void)
     }
     printf("%lu\n", total);
 
-    arr = realloc(arr, sizeof(unsigned char) * 10000);
-    total = 0;
-    for (l = 0; l < 10000; ++l) {
-        total += arr[l];
-    }
-    printf("%lu\n", total);
+// Disabled: technically, realloc could scribble over the *new* data portion and
+// leave the old data untouched, but this wasn't a project requirement so we
+// won't test for it here.
+//    arr = realloc(arr, sizeof(unsigned char) * 10000);
+//    total = 0;
+//    for (l = 0; l < 10000; ++l) {
+//        total += arr[l];
+//    }
+//    printf("%lu\n", total);
 
     puts("calloc should still zero out the memory:");
     int *cleared = calloc(sizeof(int), 450);
