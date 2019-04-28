0d2a1fc25b580297d7a976ccc5169205a6dc70b3
Matthew Malensek <matt@malensek.net>
2019-04-28 00:05:34 -0700

    Split Unix util test

--- 01-ls-1.sh
+++ 01-ls-1.sh
@@ -1,7 +1,7 @@
 source "${TEST_DIR}/funcs.bash"
 
 test_start "Unix Utilities" \
-    "Runs 'ls /', 'df', and 'w' with custom memory allocator"
+    "Runs 'ls /'  with custom memory allocator"
 
 # Check to make sure the library exists
 [[ -e "./allocator.so" ]] || test_end 1
@@ -9,8 +9,5 @@ test_start "Unix Utilities" \
 expected=$(ls /)
 actual=$(LD_PRELOAD=./allocator.so ls /) || test_end
 compare <(echo "${expected}") <(echo "${actual}") || test_end
-
-LD_PRELOAD=./allocator.so   df  || test_end
-LD_PRELOAD=./allocator.so   w   || test_end
 
 test_end
